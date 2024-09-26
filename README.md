cd8a679a70ba18bbf1d3d4d0ecbc635e7b4e8cb3
te5s3rakt <117922694+te5s3rakt@users.noreply.github.com>
2024-09-26 21:31:50 +0800

    Complete getComputerChoice function

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 **Project: Rock Paper Scissors**
 
+For this project, I will create the game Rock Paper Scissors. This game will be played entirely in the console.
--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello World")
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+};
+
+function getComputerChoice() {
+    switch(getRandomInt(3)) {
+        case 0:
+            return "r";     //rock
+            break;
+        case 1:
+            return "p";     //paper
+            break;
+        case 2:
+            return "s";     //scissors
+            break; 
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt("Choose: Rock (R), Paper (P), or Scissors (S)").charAt(0).toLowerCase();
+}
+
+console.log(getComputerChoice());
